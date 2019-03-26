27c02665546d5c8c5bee73171d39428ec15f3784
Toni Karppi <karppitoni@gmail.com>
2019-03-26 23:37:28 +0100

    Add script

---
+++ low-battery.py
@@ -0,0 +1,35 @@
+import psutil
+from datetime import datetime
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+def get_beep_sound():
+    beep_sound = AudioSegment.from_wav("sounds/beep.wav")
+    beep_sound -= 20  # Lowers the volume
+    return beep_sound
+
+
+def get_battery_info():
+    battery = psutil.sensors_battery()
+    battery_percentage = battery[0]
+    battery_plugged_in = battery[2]
+    return battery_percentage, battery_plugged_in
+
+
+def get_date_string():
+    return datetime.now().strftime("%Y-%m-%d %H:%M")
+
+
+def main():
+    date_string = get_date_string()
+    print(f"Script started on: {date_string}.")
+    battery_percentage, battery_plugged_in = get_battery_info()
+
+    if battery_percentage <= 10 and not battery_plugged_in:
+        beep_sound = get_beep_sound()
+        play(beep_sound)
+
+
+if __name__ == "__main__":
+    main()
